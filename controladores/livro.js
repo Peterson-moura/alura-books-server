2af99487634880664557d2dc35e59d207b22824a
Peterson-moura <peterson.moura@outlook.com.br>
2024-02-11 10:05:41 -0300

    criacao_getLivro_

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,8 +1,10 @@
-const fs = require("fs")
+const fs = require("fs");
+const { getTodosLivros,getLivrosPorId } = require("../servicos/livro");
+
 
 function getLivros(req, res)  {
     try {
-      const livros = JSON.parse(fs.readFileSync("livros.json"))
+      const livros = getTodosLivros()
       res.send(livros);
     } catch (error) {
       res.status(500);
@@ -10,6 +12,18 @@ function getLivros(req, res)  {
     }
   }
 
+  function getLivro(req, res)  {
+    try {
+      const id = req.params.id
+      const livro = getLivrosPorId(id)
+      res.send(livro);
+    } catch (error) {
+      res.status(500);
+      res.send(error.message)
+    }
+  }
+
   module.exports = {
-    getLivros
+    getLivros,
+    getLivro
   }
